393ee900c7dd0e1aa90011b5001e845c60b6d5af
Matt Westcott <matt@west.co.tt>
2020-04-05 02:18:26 +0100

    Refactor PSG file handling into its own module

--- client/psg.py
+++ client/psg.py
@@ -1,67 +1,28 @@
 #!/usr/bin/env python3
 
-import socket
-import struct
 import sys
 import time
 
 from papaya import Papaya, SPECTRUM_AY_FREQUENCY
+from psgfile import PSGFile
 
 
 if len(sys.argv) != 2:
     print("Usage: psg.py <psgfile>")
     sys.exit()
 
-with open(sys.argv[1], mode='rb') as f:
-    psg_data = f.read()
-
-header, = struct.unpack_from('4s', psg_data, 0)
-if header != b'PSG\x1a':
+try:
+    psg = PSGFile(sys.argv[1])
+except PSGFile.BadPSGFile:
     print("File is not a valid PSG file")
     sys.exit()
 
 ay = Papaya('192.168.1.64')
 ay.open(SPECTRUM_AY_FREQUENCY)
 
-# Read initial AY values from PSG file
-initial_values, = struct.unpack_from('12s', psg_data, 4)
-ay.write(0, enumerate(initial_values))
-
-pos1 = 16
-eof1 = False
-wait_frames1 = 0
-
-while (not eof1) or wait_frames1:
-    reg_writes1 = []
-    if eof1:
-        pass
-    elif wait_frames1:
-        wait_frames1 -= 1
-    else:
-        # read all commands for frame
-        while True:
-            if pos1 == len(psg_data):
-                eof1 = True
-                break
-
-            cmd = psg_data[pos1]
-            pos1 += 1
-            if cmd == 0xfd:
-                eof1 = True
-                break
-            elif cmd == 0xff:
-                break
-            elif cmd == 0xfe:
-                wait_frames1 = (psg_data[pos1] * 4) - 1
-                pos1 += 1
-                break
-            else:
-                reg_writes1.append((cmd, psg_data[pos1]))
-                pos1 += 1
-
-    if reg_writes1:
-        ay.write(0, reg_writes1)
-
+for frame in psg.frames():
+    if frame:
+        ay.write(0, frame)
     ay.pause(20000)
     time.sleep(0.01)
 
---
+++ client/psgfile.py
@@ -0,0 +1,55 @@
+import struct
+
+
+class PSGFile:
+    class BadPSGFile(Exception):
+        pass
+
+    def __init__(self, f):
+        if isinstance(f, str):
+            filename = f
+            with open(filename, mode='rb') as f:
+                self.data = f.read()
+        else:
+            self.data = f.read()
+
+        header, = struct.unpack_from('4s', self.data, 0)
+        if header != b'PSG\x1a':
+            raise PSGFile.BadPSGFile("File is not a valid PSG file")
+
+    def frames(self):
+        # Read initial AY values from PSG file
+        initial_values, = struct.unpack_from('12s', self.data, 4)
+        reg_writes = list(enumerate(initial_values))
+
+        pos = 16
+        eof = False
+        wait_frames = 0
+
+        while (not eof) or wait_frames:
+            if wait_frames:
+                wait_frames -= 1
+            else:
+                # read all commands for frame
+                while True:
+                    if pos == len(self.data):
+                        eof = True
+                        break
+
+                    cmd = self.data[pos]
+                    pos += 1
+                    if cmd == 0xfd:
+                        eof = True
+                        break
+                    elif cmd == 0xff:
+                        break
+                    elif cmd == 0xfe:
+                        wait_frames = (self.data[pos] * 4) - 1
+                        pos += 1
+                        break
+                    else:
+                        reg_writes.append((cmd, self.data[pos]))
+                        pos += 1
+
+            yield reg_writes
+            reg_writes = []
